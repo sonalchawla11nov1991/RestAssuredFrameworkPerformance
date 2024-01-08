995877f79778c7b2df8ca1f7e31a448df5cbc0b7
Sonal Chawla <sonal.chawla567@gmail.com>
2024-01-08 22:10:31 +0530

    First commit

--- src/main/java/apiBuilders/PostAPIBuilder.java
+++ src/main/java/apiBuilders/PostAPIBuilder.java
@@ -1,26 +1,49 @@
 package apiBuilders;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class PostAPIBuilder {
-    /*DurgeshRestAssured*/
-    public static Map<String,String > postRequestBody(String id, String title, String author){
 
-        Map<String ,String > body=new HashMap<>();
-        body.put("id",id);
-        body.put("title",title);
-        body.put("author",author);
+    public  Map<String, Object> createGoalRequestBody() {
+        Map<String, Object> body = new HashMap<>();
+
+        // Add key-value pairs for the goal data
+        body.put("startDate", "2024-10-01");
+        body.put("dueDate", "2024-12-31");
+        body.put("name", "Goal 1 for Automation");
+        body.put("description", "");
+        body.put("goalPeriod", "Q4");
+        body.put("impactArea", "Individual OKR (Impact Individual)");
+        body.put("goalAlignment", "rgG7L2Uvj9JNqAPiixUCWAe1Q2u3A4le1Q2u3A4l");
+        body.put("ownerId", "1g2Ls1L2a3S4hvBEJRZ61gp1L2u3SAna6hkge1Q2u3A4le1Q2u3A4l");
+        body.put("goalType", "INDIVIDUAL");
+        body.put("weightage", 100);
+        body.put("goalAlignmentType", "COMPANY_GOAL");
+        body.put("autoRatingList", null);
+
+        // Add milestone list
+        Map<String, Object> milestone = new HashMap<>();
+        milestone.put("name", "Milestone");
+        milestone.put("typeOfKR", "OUTCOME");
+        milestone.put("progressType", "METRIC");
+        milestone.put("startDate", "2023-10-01");
+        milestone.put("dueDate", "2023-12-31");
+        milestone.put("ownerId", "1g2Ls1L2a3S4hvBEJRZ61gp1L2u3SAna6hkge1Q2u3A4le1Q2u3A4l");
+        milestone.put("uom", "INR in lacs");
+        milestone.put("typeOfOutcome", "4x Pipeline Value(Specific to commercial)");
+        milestone.put("weightage", 100);
+        milestone.put("startValue", 0);
+        milestone.put("targetValue", 100);
+        milestone.put("targetCondition", "AT_LEAST");
+        milestone.put("autoRatingList", null);
+
+        body.put("milestoneList", List.of(milestone));
+        body.put("goalRatingGuidelines", null);
+        body.put("goalSettingId", "QkfbZOStbj7p1L2u3SLYbGPs1L2a3S4ha8Zge1Q2u3A4le1Q2u3A4l");
 
         return body;
-    }
-    public Map<String ,String > postValidationBody(){
-        Map<String ,String > bodyValPost=new HashMap<>();
-        bodyValPost.put("id","7");
-        bodyValPost.put("title","Geeta");
-        bodyValPost.put("author","Ved Vyash");
-
-        return bodyValPost;
     }
 
 }
--- src/main/java/apiConfigs/APIPath.java
+++ src/main/java/apiConfigs/APIPath.java
@@ -2,21 +2,13 @@ package apiConfigs;
 
 public class APIPath {
     public static final class apiPath {
-
-        //END POINTS FOR API
-        //get list of users
-        public static final String GET_LIST_OF_USERS="posts";
-        //get single user
-        public static final String SINGLE_USER="posts/1";
-        //post create user
-        public static final String  CREATE_USER="posts";
         //delete user
-        public static final String DELETE_USER="posts";
+        public static final String CREATE_GOAL="service/oneweb/api/goal/employee/createGoal";
 
     }
 
     public static void main(String[] args) {
-        System.out.println(APIPath.apiPath.CREATE_USER);
-        System.out.println(apiPath.GET_LIST_OF_USERS);
+//        System.out.println(APIPath.apiPath.CREATE_USER);
+//        System.out.println(apiPath.GET_LIST_OF_USERS);
     }
 }
